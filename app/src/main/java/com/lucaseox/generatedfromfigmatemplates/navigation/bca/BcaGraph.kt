553d78d2e9ab797e8cac3c8891c210bf7c9b1e3e
Lucas Xavier <lucaseoxavier@gmail.com>
2025-01-13 18:33:39 -0300

    feat: complete BCA graph

--- app/src/main/java/com/lucaseox/generatedfromfigmatemplates/navigation/bca/BcaGraph.kt
+++ app/src/main/java/com/lucaseox/generatedfromfigmatemplates/navigation/bca/BcaGraph.kt
@@ -12,6 +12,27 @@ fun NavGraphBuilder.bcaGraph(navController: NavHostController) {
     navigation<Router.BCA.Route>(startDestination = Router.BCA.MenuScreen) {
         composable<Router.BCA.MenuScreen> {
             GridScreen(navController, bcaMenuButtons)
+            composable<Router.BCA.LoginScreen> {}
+            composable<Router.BCA.HomeScreen> {}
+            composable<Router.BCA.ScanQrScreen> {}
+            composable<Router.BCA.ShowQrScreen> {}
+            composable<Router.BCA.TransactionScreen> {}
+            composable<Router.BCA.FullTransactionScreen> {}
+            composable<Router.BCA.FlazzScreen> {}
+            composable<Router.BCA.FlazzBalanceScreen> {}
+            composable<Router.BCA.FlazzTopUpScreen> {}
+            composable<Router.BCA.FlazzCompletedScreen> {}
+            composable<Router.BCA.InfoScreen> {}
+            composable<Router.BCA.NotificationScreen> {}
+            composable<Router.BCA.MessageScreen> {}
+            composable<Router.BCA.ReceiverScreen> {}
+            composable<Router.BCA.AmountScreen> {}
+            composable<Router.BCA.TransferCompletedScreen> {}
+            composable<Router.BCA.NewAccountScreen> {}
+            composable<Router.BCA.ProfileScreen> {}
+            composable<Router.BCA.ControlScreen> {}
+            composable<Router.BCA.SetLimitScreen> {}
+            composable<Router.BCA.BlockScreen> {}
         }
     }
 }
