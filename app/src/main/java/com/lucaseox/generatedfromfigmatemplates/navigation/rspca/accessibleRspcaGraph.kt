2fda684974431789e8ab6f649115d15e55c5da77
Lucas Xavier <lucaseoxavier@gmail.com>
2025-01-13 18:45:36 -0300

    feat: complete RSPCAgraph

--- app/src/main/java/com/lucaseox/generatedfromfigmatemplates/navigation/rspca/accessibleRspcaGraph.kt
+++ app/src/main/java/com/lucaseox/generatedfromfigmatemplates/navigation/rspca/accessibleRspcaGraph.kt
@@ -12,6 +12,25 @@ fun NavGraphBuilder.accessibleRspcaGraph(navController: NavHostController) {
     navigation<Router.AccessibleRSPCA.Route>(startDestination = Router.AccessibleRSPCA.MenuScreen) {
         composable<Router.AccessibleRSPCA.MenuScreen> {
             GridScreen(navController, accessibleRSPCAMenuButtons)
+            composable<Router.AccessibleRSPCA.StartScreen> {}
+            composable<Router.AccessibleRSPCA.LoginScreen> {}
+            composable<Router.AccessibleRSPCA.HomeScreen> {}
+            composable<Router.AccessibleRSPCA.RegisterPetScreen> {}
+            composable<Router.AccessibleRSPCA.DogScreen> {}
+            composable<Router.AccessibleRSPCA.RegisterDogScreen> {}
+            composable<Router.AccessibleRSPCA.AdoptionScreen> {}
+            composable<Router.AccessibleRSPCA.AdoptionFormScreen> {}
+            composable<Router.AccessibleRSPCA.ServicesScreen> {}
+            composable<Router.AccessibleRSPCA.CurrentPetsScreen> {}
+            composable<Router.AccessibleRSPCA.VetScreen> {}
+            composable<Router.AccessibleRSPCA.GroomingScreen> {}
+            composable<Router.AccessibleRSPCA.MapScreen> {}
+            composable<Router.AccessibleRSPCA.PetInfoScreen> {}
+            composable<Router.AccessibleRSPCA.DietScreen> {}
+            composable<Router.AccessibleRSPCA.BMIScreen> {}
+            composable<Router.AccessibleRSPCA.ExerciseScreen> {}
+            composable<Router.AccessibleRSPCA.WalkScreen> {}
+            composable<Router.AccessibleRSPCA.WalkRecordScreen> {}
         }
     }
 }
--- app/src/main/java/com/lucaseox/generatedfromfigmatemplates/navigation/rspca/rspcaGraph.kt
+++ app/src/main/java/com/lucaseox/generatedfromfigmatemplates/navigation/rspca/rspcaGraph.kt
@@ -13,5 +13,24 @@ fun NavGraphBuilder.rspcaGraph(navController: NavHostController) {
         composable<Router.RSPCA.MenuScreen> {
             GridScreen(navController, RSPCAMenuButtons)
         }
+        composable<Router.RSPCA.StartScreen> {}
+        composable<Router.RSPCA.LoginScreen> {}
+        composable<Router.RSPCA.HomeScreen> {}
+        composable<Router.RSPCA.RegisterPetScreen> {}
+        composable<Router.RSPCA.DogScreen> {}
+        composable<Router.RSPCA.RegisterDogScreen> {}
+        composable<Router.RSPCA.AdoptionScreen> {}
+        composable<Router.RSPCA.AdoptionFormScreen> {}
+        composable<Router.RSPCA.ServicesScreen> {}
+        composable<Router.RSPCA.CurrentPetsScreen> {}
+        composable<Router.RSPCA.VetScreen> {}
+        composable<Router.RSPCA.GroomingScreen> {}
+        composable<Router.RSPCA.MapScreen> {}
+        composable<Router.RSPCA.PetInfoScreen> {}
+        composable<Router.RSPCA.DietScreen> {}
+        composable<Router.RSPCA.BMIScreen> {}
+        composable<Router.RSPCA.ExerciseScreen> {}
+        composable<Router.RSPCA.WalkScreen> {}
+        composable<Router.RSPCA.WalkRecordScreen> {}
     }
 }
